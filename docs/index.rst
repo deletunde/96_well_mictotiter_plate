119bad698b163f3c173e78cff48f1a8cd90eac98
Emmanuel Akano <deletunde@gmail.com>
2019-01-01 15:10:53 -0500

    Initial commit

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. 96 well mictotiter plate documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+96 well mictotiter plate documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project for the NIH FAES BIOF309 (Introduction to Python Programming) class. The overall goal of the project is to create better workflows for working with 96-well plates. This involves creating python workflows for data import, data wrangling, and exploratory data analysis. In the future, I hope to adapt the code to other types of microtiter plates. ',
+    author='Emmanuel Akano',
+    license='MIT',
+)
